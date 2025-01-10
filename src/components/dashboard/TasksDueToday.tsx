8b4cf210727429d8a8f440fb529a267bf6e426e2
karlpnord <karnor50544@stud.noroff.no>
2025-01-10 13:05:15 +0100

    Typescript error fix

--- src/components/dashboard/TasksDueToday.tsx
+++ src/components/dashboard/TasksDueToday.tsx
@@ -9,11 +9,11 @@ const TasksDueToday = ({ toggleSettings }: Props) => {
     <div>
       <h2 className='text-2xl font-bold mb-2'>Tasks due today</h2>
       <ul className='bg-zinc-900 p-4 overflow-y-scroll rounded-md h-40'>
-        <Task title='Task 1' toggleSettings={toggleSettings} />
-        <Task title='Task 2' toggleSettings={toggleSettings} />
-        <Task title='Task 3' toggleSettings={toggleSettings} />
-        <Task title='Task 4' toggleSettings={toggleSettings} />
-        <Task title='Task 5' toggleSettings={toggleSettings} />
+        <Task title='Task 1' isImportant={false} toggleSettings={toggleSettings} />
+        <Task title='Task 2' isImportant={false} toggleSettings={toggleSettings} />
+        <Task title='Task 3' isImportant={false} toggleSettings={toggleSettings} />
+        <Task title='Task 4' isImportant={false} toggleSettings={toggleSettings} />
+        <Task title='Task 5' isImportant={false} toggleSettings={toggleSettings} />
       </ul>
     </div>
   );
