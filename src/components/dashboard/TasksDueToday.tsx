e9a3990482110109a06e88bf2731a4c7ed427388
karlpnord <karnor50544@stud.noroff.no>
2025-01-11 16:03:13 +0100

    Typescript fix

--- src/components/dashboard/TasksDueToday.tsx
+++ src/components/dashboard/TasksDueToday.tsx
@@ -1,19 +1,20 @@
 import Task from '../ui/Task';
+import useTaskStore from '../../store/taskStore';
 
 interface Props {
   toggleSettings: () => void;
 }
 
 const TasksDueToday = ({ toggleSettings }: Props) => {
+  const { myDayTasks } = useTaskStore();
+  
   return (
     <div>
       <h2 className='text-2xl font-bold mb-2'>Tasks due today</h2>
       <ul className='bg-zinc-900 p-4 overflow-y-scroll rounded-md h-40'>
-        <Task title='Task 1' isImportant={false} toggleSettings={toggleSettings} />
-        <Task title='Task 2' isImportant={false} toggleSettings={toggleSettings} />
-        <Task title='Task 3' isImportant={false} toggleSettings={toggleSettings} />
-        <Task title='Task 4' isImportant={false} toggleSettings={toggleSettings} />
-        <Task title='Task 5' isImportant={false} toggleSettings={toggleSettings} />
+        {myDayTasks.map((task) => (
+          <Task key={task.id} task={task} toggleSettings={toggleSettings} />
+        ))}
       </ul>
     </div>
   );
